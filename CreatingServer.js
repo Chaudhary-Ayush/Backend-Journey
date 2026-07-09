2b9b7d989db189a228862f7201531e839c88b36f
Ayush Chaudhary <acgadasia2002@gmail.com>
2026-07-09 14:27:11 +0530

    Playing with Public APIs

--- CreatingServer.js
+++ CreatingServer.js
@@ -1,7 +1,6 @@
 // How to create server
 const http = require('http');
 
-
 const server = http.createServer((req, res)=>{
     res.end("First Server Created");
 }) ;  // Here, req and res, both are objects. 
